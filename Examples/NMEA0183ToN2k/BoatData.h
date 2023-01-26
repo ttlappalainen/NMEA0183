da97649f7d0165e466a0c8e46be8bfd036f25b1d
AndrasSzep <andras.szep@gmail.com>
2023-01-26 10:40:31 +0000

    additional params boatdata

--- Examples/NMEA0183ToN2k/BoatData.h
+++ Examples/NMEA0183ToN2k/BoatData.h
@@ -3,30 +3,34 @@
 
 struct tBoatData {
   unsigned long DaysSince1970;   // Days since 1970-01-01
-  
-  double TrueHeading,SOG,COG,Variation,
-         GPSTime,// Secs since midnight,
-         Latitude, Longitude, Altitude, HDOP, GeoidalSeparation, DGPSAge;
+
+  double TrueHeading, SOG, COG, Variation,
+      GPSTime, // Secs since midnight,
+      Latitude, Longitude, Altitude, HDOP, GeoidalSeparation, DGPSAge,
+      WaterTemperature, WaterDepth, Offset,
+      WindDirectionT, WindDirectionM, WindSpeedK, WindSpeedM,
+      WindAngle;
   int GPSQualityIndicator, SatelliteCount, DGPSReferenceStationID;
   bool MOBActivated;
   char Status;
 
-public:
-  tBoatData() {
-    TrueHeading=0;
-    SOG=0;
-    COG=0; 
-    Variation=7.0;
-    GPSTime=0;
+  tBoatData()
+  {
+    TrueHeading = 0;
+    SOG = 0;
+    COG = 0;
+    Variation = 7.0;
+    GPSTime = 0;
     Latitude = 0;
     Longitude = 0;
-    Altitude=0;
-    HDOP=100000;
-    DGPSAge=100000;
-    DaysSince1970=0; 
-    MOBActivated=false; 
-    SatelliteCount=0; 
-    DGPSReferenceStationID=0;
+    Altitude = 0;
+    HDOP = 100000;
+    DGPSAge = 100000;
+    WaterTemperature = 0;
+    DaysSince1970 = 0;
+    MOBActivated = false;
+    SatelliteCount = 0;
+    DGPSReferenceStationID = 0;
   };
 };
 
